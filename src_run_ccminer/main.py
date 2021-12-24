d7bbfc4a5e88ed93d7530a88a2442658187fa269
creativeHHD <92228259+creativeHHD@users.noreply.github.com>
2021-12-24 13:26:14 +0700

    Add files via upload

--- src_run_ccminer/main.py
+++ src_run_ccminer/main.py
@@ -94,20 +94,6 @@ def set_miner():
 
 while True:
     os.system("clear")
-    print("\n"
-      + "\n"
-        + "\n"
-          + "\n"
-            + "\n"
-              + "\n"
-                + "\n"
-                  + "\n"
-                    + "\n"
-                      + "\n"
-                        + "\n"
-                          + "\n"
-                            + "\n"
-                              + "\n")
    with MoonSpinner("                  รอสักครู่...") as bar:
         for i in range(100):
             time.sleep(0.05)
